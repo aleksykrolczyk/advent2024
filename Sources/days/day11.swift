f3ffbe11f61e37198a0f200ab09f5e3a8d341d48
aleksykrolczyk <aleksykrolczyk@yahoo.pl>
2024-12-11 12:32:44 +0100

    day11

---
+++ Sources/days/day11.swift
@@ -0,0 +1,73 @@
+class Day11: AdventDay {
+
+    static var memo: [String: Int] = [:]
+
+    func transform(stone: Int) -> [Int] {
+        if stone == 0 {
+            return [1]
+        }
+        if stone.length % 2 == 0 {
+            let (left, right) = stone.split()
+            return [left, right]
+        }
+        return [stone * 2024]
+    }
+
+    func blink(stone: Int, n: Int = 1) -> Int {
+        let key = "\(stone)-\(n)"
+        if let hit = Self.memo[key] {
+            return hit
+        }
+
+        if n == 0 {
+            return stone
+        }
+
+        var result: Int
+        if n == 1 {
+            result = transform(stone: stone).count
+        } else {
+            result = transform(stone: stone)
+                .map { blink(stone: $0, n: n - 1) }
+                .reduce(0, +)
+        }
+
+        Self.memo[key] = result
+        return result
+    }
+
+    func part1() -> Any {
+        let stones = Self.dataLines.first!.toInts(separator: " ")
+        return
+            stones
+            .map { blink(stone: $0, n: 25) }
+            .reduce(0, +)
+    }
+
+    func part2() -> Any {
+        let stones = Self.dataLines.first!.toInts(separator: " ")
+        return
+            stones
+            .map { blink(stone: $0, n: 75) }
+            .reduce(0, +)
+    }
+}
+
+extension Int {
+    var length: Int {
+        var (c, length) = (self, 0)
+        while c > 0 {
+            length += 1
+            c = c / 10
+        }
+        return length
+    }
+
+    func split() -> (Int, Int) {
+        let str = String(self)
+        let mid = str.index(str.startIndex, offsetBy: length / 2)
+        let left = Int(String(str[..<mid]))!
+        let right = Int(String(str[mid...]))!
+        return (left, right)
+    }
+}
